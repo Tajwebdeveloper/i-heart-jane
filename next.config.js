89b27b0fa9846c1c7dd608e4d5588676b16157d6
Tajuddin <khantaj65@gmail.com>
2022-03-29 11:26:09 +0530

    removed caching in next.jsupdate

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 module.exports = {
   generateEtags: false,
   async headers() {
-    return [
-      {
-        source: '/',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
-          }
-        ],
-      },
-    ]
-  },
+  return [
+    {
+      source: '/:all*(svg|jpg|png)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=-1, must-revalidate',
+        }
+      ],
+    },
+  ]
+},
 }
